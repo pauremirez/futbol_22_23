6ead83f89973da2e871453ae517a028d24618825
pauremirez <5580416+pauremirez@users.noreply.github.com>
2023-05-10 15:22:52 +0200

    Update Persona.java

--- futbol_temporada_22_23/src/futbol/Persona.java
+++ futbol_temporada_22_23/src/futbol/Persona.java
@@ -1,6 +1,6 @@
 package futbol;
 
 public class Persona {
-    // TODO 3: Implementa la clase jugador correctamente, de acuerdo al diagrama de clases siguiente: 
+    // TODO 3: Implementa la clase persona correctamente, de acuerdo al diagrama de clases siguiente: 
 URL_diagrama_de_clases
 }
