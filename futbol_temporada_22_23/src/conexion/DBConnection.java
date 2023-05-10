7f6842555fed2bdb23792ede48b3d8ae64793db8
pauremirez <5580416+pauremirez@users.noreply.github.com>
2023-05-10 15:21:50 +0200

    Update DBConnection.java

--- futbol_temporada_22_23/src/conexion/DBConnection.java
+++ futbol_temporada_22_23/src/conexion/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 
 public class DBConnection {
 	public String db="futbol_temporada_22_23";
-	public String url="jdbc:mysql://localhost:8889/"+db+"?serverTimezone=UTC";
+	public String url="jdbc:mysql://localhost:3360/"+db+"?serverTimezone=UTC";
 	public String user="root";
 	public String pass="root";
         
